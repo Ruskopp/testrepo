be2ebf4f2a67c8af7cbfe5adc3130151c7a4fa23
Ruskopp <marko.r.kastratovic@gmail.com>
2016-04-24 19:22:37 +0200

    Ubacen Doctrine, podesena default baza, uz pomoc Doctrine na osnovu baze automatski generisani entiteti. (Marija i ja radili <- Drazenu komentar =)

---
+++ application/libraries/Doctrine.php
@@ -0,0 +1,93 @@
+<?php
+
+use Doctrine\Common\ClassLoader,
+    Doctrine\ORM\Tools\Setup,
+    Doctrine\ORM\Configuration,
+    Doctrine\ORM\EntityManager,
+    Doctrine\Common\Cache\ArrayCache,
+    Doctrine\DBAL\Logging\EchoSQLLogger,
+    Doctrine\ORM\Mapping\Driver\DatabaseDriver,
+    Doctrine\ORM\Tools\DisconnectedClassMetadataFactory,
+    Doctrine\ORM\Tools\EntityGenerator;
+
+/**
+ * Doctrine bootstrap library for CodeIgniter
+ *
+ * @author	Joseph Wynn <joseph@wildlyinaccurate.com>
+ * @link	http://wildlyinaccurate.com/integrating-doctrine-2-with-codeigniter-2
+ */
+class Doctrine
+{
+
+	public $em;
+
+	public function __construct()
+	{
+		require_once __DIR__ . '/Doctrine/ORM/Tools/Setup.php';
+		Setup::registerAutoloadDirectory(__DIR__);
+
+		// Load the database configuration from CodeIgniter
+		require APPPATH . 'config/database.php';
+
+		$connection_options = array(
+			'driver'		=> 'pdo_mysql',
+			'user'			=> $db['default']['username'],
+			'password'		=> $db['default']['password'],
+			'host'			=> $db['default']['hostname'],
+			'dbname'		=> $db['default']['database'],
+			'charset'		=> $db['default']['char_set'],
+			'driverOptions'	=> array(
+				'charset'	=> $db['default']['char_set'],
+			),
+		);
+
+		// With this configuration, your model files need to be in application/models/Entity
+		// e.g. Creating a new Entity\User loads the class from application/models/Entity/User.php
+		$models_namespace = 'Entity';
+		$models_path = APPPATH . 'models';
+		$proxies_dir = APPPATH . 'models/Proxies';
+		$metadata_paths = array(APPPATH . 'models');
+
+		// Set $dev_mode to TRUE to disable caching while you develop
+		$dev_mode = true;
+
+		// If you want to use a different metadata driver, change createAnnotationMetadataConfiguration
+		// to createXMLMetadataConfiguration or createYAMLMetadataConfiguration.
+		$config = Setup::createAnnotationMetadataConfiguration($metadata_paths, $dev_mode, $proxies_dir);
+		$this->em = EntityManager::create($connection_options, $config);
+
+		$loader = new ClassLoader($models_namespace, $models_path);
+		$loader->register();
+                
+              //$this->generate_classes(); 
+	}
+        
+        
+       public function generate_classes(){     
+       
+		$this->em->getConfiguration()
+             ->setMetadataDriverImpl(
+                new DatabaseDriver(
+                        $this->em->getConnection()->getSchemaManager()
+                )
+		);
+ 
+    $cmf = new DisconnectedClassMetadataFactory();
+    $cmf->setEntityManager($this->em);
+    $metadata = $cmf->getAllMetadata();     
+    $generator = new EntityGenerator();
+     
+    $generator->setUpdateEntityIfExists(true);
+    $generator->setGenerateStubMethods(true);
+    $generator->setGenerateAnnotations(true);
+    $generator->generate($metadata, APPPATH."models/Entities");
+     
+  }
+  
+        
+        
+
+}
+
+  
+  
