e34f1edb4d65b14d6c9e75c61d0e80ffe5c8fcf0
Ruskopp <marko.r.kastratovic@gmail.com>
2016-05-27 20:32:04 +0200

    baze url promenjen

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://localhost/PSI_Implementacija/';
+//$config['base_url'] = 'http://localhost/PSI_Implementacija/';
+$config['base_url'] = 'http://http://restooking.azurewebsites.net//PSI_Implementacija/';
 
 /*
   |--------------------------------------------------------------------------
